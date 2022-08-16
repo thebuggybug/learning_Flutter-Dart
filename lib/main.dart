c250d6f26758e4d66a0f0234d0500d078d862e12
thebuggybug <rohansayami@gmail.com>
2022-08-16 22:54:53 +0545

    forms, issue

--- lib/main.dart
+++ lib/main.dart
@@ -14,153 +14,89 @@ class MyApp extends StatelessWidget {
       title: " My Application",
       theme: ThemeData.dark(),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Android Application"),
-        ),
-
-        // body: Stack(
-        //   alignment: Alignment.center,
-        //   textDirection: TextDirection.rtl,
-        //   fit: StackFit.passthrough,
-        //
-        //
-        //   children: [
-        //     Center(
-        //       child: Container(
-        //         // alignment: ,
-        //         height: 500,
-        //         width: 350,
-        //         color: Colors.green,
-        //       ),
-        //     ),
-        //     Container(
-        //       height: 450,
-        //       width: 300,
-        //       color: Colors.white,
-        //     ),
-        //     Container(
-        //       height: 400,
-        //       width: 250,
-        //       color: Colors.redAccent,
-        //     ),
-        //
-        //     Container(
-        //       decoration: BoxDecoration(
-        //           color: Colors.orangeAccent,
-        //           borderRadius: BorderRadius.circular(50)
-        //       ),
-        //       height: 100,
-        //       width: 100,
-        //     ),
-        //   ],
-        // ),
-
-        body: Center(
-          child: Stack(
-            fit: StackFit.passthrough,
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(400),
-                  color: Colors.orangeAccent,
-
-                ),
-                height: 300,
-                width: 300,
-                child: Center(
-                  child: Text(
-                    "HEY",
-                    style: TextStyle(
-                        fontSize: 40,
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
-              Positioned(
-                top: 10,
-                // left: 0,
-                right: 10,
-                child: Container(
-                  height: 70,
-                  width: 70,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50),
-                    color: Colors.blue,
-                  ),
-                  child: Center(
-                    child: Text(
-                      "Blue",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ),
-
-                ),
-              ),
-              Positioned(
-                top: 10,
-                // left: 0,
-                left: 10,
-                child: Container(
-                  height: 70,
-                  width: 70,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50),
-                    color: Colors.red,
-                  ),
-                  child: Center(
-                    child: Text(
-                      "Red",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ),
-                ),
-              ),
-
-              Positioned(
-                bottom: 10,
-                // left: 0,
-                right: 10,
-                child: Container(
-                  height: 70,
-                  width: 70,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50),
-                    color: Colors.green,
-                  ),
-                  child: Center(
-                    child: Text(
-                      "Green",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ),
-                ),
-              ),
-
-              Positioned(
-                bottom: 10,
-                // left: 0,
-                left: 10,
-                child: Container(
-                  height: 70,
-                  width: 70,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50),
-                    color: Colors.deepPurple,
-                  ),
-                  child: Center(
-                    child: Text(
-                      "Purple",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ),
-
-                ),
-              ),
-            ],
+          appBar: AppBar(
+            title: Text("Android Application FORMS"),
           ),
-        ),
-      ),
+          body: Forms()),
     );
+  }
+}
+
+class Forms extends StatefulWidget {
+  const Forms({Key? key}) : super(key: key);
+
+  @override
+  State<Forms> createState() => _FormsState();
+}
+
+class _FormsState extends State<Forms> {
+  final keys = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        key: keys,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                  hintText: "Provide Name",
+                  labelText: "Name",
+                  icon: Icon(
+                    Icons.supervised_user_circle_outlined,
+                    color: Colors.orange,
+                  )),
+              validator: (String? value) {
+                if (value != null && value.isEmpty) {
+                  return "Username can't be empty";
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  hintText: "Your Password",
+                  labelText: "Password",
+                  icon: Icon(
+                    Icons.password,
+                    color: Colors.pink,
+                  )),
+              validator: (String? value) {
+                if (value != null && value.isEmpty) {
+                  return "Password can't be empty";
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  hintText: "Social Handle here",
+                  labelText: "Link",
+                  icon: Icon(
+                    Icons.link_rounded,
+                    color: Colors.orange,
+                  )),
+              validator: (String? value) {
+                if (value != null && value.isEmpty) {
+                  return "LINKS can't be empty";
+                }
+                return null;
+              },
+            ),
+
+            // issue
+            // Container(
+            //     child: OutlinedButton(
+            //   onPressed: () {
+            //     if(keys.currentState.validate())
+            //       {
+            //         Scaffold.of(context).showSnackBar(SnackBar(content: Text("Login"),));
+            //       }
+            //   },
+            //   child: Text("Submit"),
+            // ))
+          ],
+        ));
   }
 }
