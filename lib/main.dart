bea7cf10f6c00a19e8a70c5cdcf703985c59a3b6
thebuggybug <rohansayami@gmail.com>
2022-08-16 19:05:57 +0545

    created a small Login Ui | useed diff. buttons

--- lib/main.dart
+++ lib/main.dart
@@ -10,115 +10,177 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(brightness: Brightness.dark, primaryColor: Colors.red),
         title: "My application",
         home: Scaffold(
-          backgroundColor: Colors.green,
-          appBar: AppBar(
-            title: Text("Flutter Dev ",
-                style: TextStyle(color: Colors.amberAccent)),
-            leading: Icon(
-              Icons.account_box,
-              size: 28,
-              color: Colors.pink,
+            backgroundColor: Colors.indigoAccent,
+            appBar: AppBar(
+              backgroundColor: Colors.red,
+              centerTitle: true,
+              title: Text("Demo App",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  )),
+              leading: Icon(Icons.supervised_user_circle,
+                  size: 40, color: Colors.greenAccent),
             ),
-            backgroundColor: Colors.black,
-          ),
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            // mainAxisAlignment: MainAxisAlignment.spaceAround,
-            // mainAxisAlignment: MainAxisAlignment.start,
-            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            // crossAxisAlignment: CrossAxisAlignment.baseline
-
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Container(
-                // width: 60,
-                // height: 60,
-                decoration: BoxDecoration(
-                  // borderRadius: BorderRadius.circular(50),
-
-                  color: Colors.amber, // container border
-
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.pink, spreadRadius: 8,
-                      // offset: Offset(12.2, 9.0)
+            body: Column(
+              children: [
+                Center(
+                  child: Container(
+                    height: 120,
+                    width: 120,
+                    margin: EdgeInsets.only(top: 20),
+                    // padding: EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                        color: Colors.greenAccent,
+                        borderRadius: BorderRadius.circular(75),
+                        border: Border.all(color: Colors.black, width: 4)),
+                    child: Center(
+                      child: (Text(
+                        "R",
+                        style: TextStyle(
+                            fontSize: 75,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold),
+                      )),
                     ),
-                  ],
+                  ),
                 ),
-                margin: EdgeInsets.all(25),
-                padding: EdgeInsets.all(10),
-                alignment: Alignment.center,
-                child: Text(
-                    "Learn Flutter day by day and become a professional",
-                    style: TextStyle(
-                        color: Colors.blue,
-                        fontSize: 20,
-                        fontStyle: FontStyle.italic,
-                        fontWeight: FontWeight.w800,
-                        backgroundColor: Colors.white70,
-                        letterSpacing: 1.1,
-                        wordSpacing: 2,
-                        shadows: [
-                          Shadow(color: Colors.blue, offset: Offset(1.1, 1.2))
-                        ])),
-              ),
-              // Rich Text; for applying styles in different texts
-              Container(
-                decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(14)),
-                padding: EdgeInsets.all(10),
-                child: (RichText(
-                  text: TextSpan(
-                      text: "Learning Rich Text",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
-                      children: <TextSpan>[
-                        TextSpan(
-                            text: " Heree we go ",
-                            style: TextStyle(
-                                fontWeight: FontWeight.normal,
-                                fontStyle: FontStyle.italic,
-                                color: Colors.brown))
-                      ]),
-                )),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                    color: Colors.purple,
-                    borderRadius: BorderRadius.circular(16)),
-                margin: EdgeInsets.all(12),
-                padding: EdgeInsets.all(16.0),
-                child: (TextField(
-                  // obscureText: true, // hides input password
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: " NAME",
-                      hintText: "Enter Your name here"),
-                )),
-              ),
 
-              Container(
-                decoration: BoxDecoration(
-                    color: Colors.greenAccent,
-                    borderRadius: BorderRadius.circular(16)),
-                margin: EdgeInsets.all(12),
-                padding: EdgeInsets.all(16.0),
-                child: (TextField(
-                  obscureText: true, // hides input password
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: " PASSWORD",
-                      hintText: "Enter password"),
-                )),
-              )
-            ],
-          ),
-        ));
+                //username
+                Container(
+                  margin: EdgeInsets.all(10),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      // border: Border.all(),
+                      borderRadius: BorderRadius.circular(100),
+                      color: Colors.orangeAccent),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: TextField(
+                      enableSuggestions: true,
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Your name here",
+                          icon: Icon(Icons.person)),
+                    ),
+                  ),
+                ),
+
+                //Password
+                Container(
+                  margin: EdgeInsets.all(10),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      // border: Border.all(),
+                      borderRadius: BorderRadius.circular(100),
+                      color: Colors.red),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: TextField(
+                      obscureText: true,
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Password here",
+                          icon: Icon(Icons.password)),
+                    ),
+                  ),
+                ),
+
+                //Social Links
+                Container(
+                  margin: EdgeInsets.all(10),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      // border: Border.all(),
+                      borderRadius: BorderRadius.circular(100),
+                      color: Colors.deepPurpleAccent),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: TextField(
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Social Links",
+                          icon: Icon(Icons.link)),
+                    ),
+                  ),
+                ),
+
+                // text button
+                TextButton(
+                  child: Text(
+                    "Text Btn",
+                    style: TextStyle(color: Colors.red),
+                  ),
+                  style: ButtonStyle(
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                  ),
+                  onPressed: () {},
+                ),
+
+                ElevatedButton(
+                    child: Text("Elevated Btn"),
+                    style: ButtonStyle(
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.white),
+                    ),
+                    // style: ButtonStyle(backgroundColor: ),
+                    onPressed: () {}),
+
+                OutlinedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Outline Btn",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+
+                FloatingActionButton(
+                  onPressed: () {},
+                  // child: Text("Float"),
+                  child: Icon(Icons.save),
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.red,
+                ),
+
+                FloatingActionButton.extended(
+                  onPressed: () {},
+                  label: Text("Float Extd"),
+                  icon: Icon(Icons.save),
+                  foregroundColor: Colors.black,
+                  backgroundColor: Colors.white,
+                )
+
+                //Button
+                // Container(
+                //   height: 45,
+                //   width: 200,
+                //   margin: EdgeInsets.all(10),
+                //   alignment: Alignment.center,
+                //   decoration: BoxDecoration(
+                //       // border: Border.all(),
+                //       borderRadius: BorderRadius.circular(100),
+                //       color: Colors.deepPurple),
+                //   child: Center(
+                //     child: Text(
+                //       "Update",
+                //       style:
+                //           TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                //     ),
+                //   ),
+                // ),
+
+                // >> BUTTON ) Flat Button
+              ],
+            )));
   }
 }
